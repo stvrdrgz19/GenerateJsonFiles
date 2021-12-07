a1988a20f0dbb090b40beb5090e033877a773135
Steve Rodriguez <steve.rodriguez@Cavallo.com>
2021-12-07 16:02:52 -0500

    first commit

---
+++ CreateJsonFiles/EnvMgrLiteSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJsonFiles
+{
+    public class EnvMgrLiteSettings
+    {
+        public string DatabaseBackupDirectory { get; set; }
+        public string SalesPadx86Directory { get; set; }
+        public string SalesPadx64Directory { get; set; }
+    }
+}
---
+++ CreateJsonFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateJsonFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void GenerateEnvMgrSettingsFile()
+            {
+                var envMgrLiteSettings = new EnvMgrLiteSettings
+                {
+                    DatabaseBackupDirectory = @"C:\DatabaseBackups",
+                    SalesPadx86Directory = @"C:\Program Files (x86)\SalesPad.Desktop",
+                    SalesPadx64Directory = @"C:\Program Files\SalesPad.Desktop"
+                };
+
+                string output = JsonConvert.SerializeObject(envMgrLiteSettings, Formatting.Indented);
+                File.WriteAllText(@"C:\Users\steve.rodriguez\Downloads\EnvMgrLite\Settings.json", output);
+            }
+
+            GenerateEnvMgrSettingsFile();
+        }
+    }
+}
